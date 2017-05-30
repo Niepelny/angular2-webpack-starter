aea2bcb77c8510699247ce411c4fae6865bfb860
Mateusz Biel <Mateusz Biel>
2017-05-31 01:56:17 +0200

    task8 - custom validation

---
+++ src/app/core/directives/dateValidator.directive.ts
@@ -0,0 +1,37 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  OnInit
+} from '@angular/core';
+
+import { AbstractControl, Validator } from '@angular/forms';
+
+import { DatePipe } from '@angular/common';
+@Directive({
+  selector: '[dateValidator]',
+  providers: []
+})
+export class DateValidator implements Validator, OnChanges {
+  @Input() forbiddenName: string;
+  // private valFn = Validators.nullValidator;
+  ngOnChanges(changes: SimpleChanges): void {
+    const change = changes['dateValidator'];
+    if (change) {
+      const val: string = change.currentValue;
+      // this.valFn = this.emailValidator(val);
+    } else {
+      // this.valFn = Validators.nullValidator;
+    }
+  }
+  validate(control: AbstractControl): { [key: string]: any } {
+    if (control.value.match(/[0-9{2}].[0-9{2}].[0-9{4}]/)) {
+      return null;
+    } else {
+      return {'invalidEmailAddress': true };
+    }
+  }
+
+}
